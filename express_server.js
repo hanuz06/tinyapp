b2390412827e2f55114e7304a1c3b93e33c4a0c1
hanuz06 <lee1975uz@gmail.com>
2019-10-28 21:38:26 +0000

    setup "views" folder with partials and pages

--- express_server.js
+++ express_server.js
@@ -1,23 +1,51 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; // default port 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// index page 
+// index page 
+app.get('/', function (req, res) {
+  var drinks = [{
+      name: 'Bloody Mary',
+      drunkness: 3
+    },
+    {
+      name: 'Martini',
+      drunkness: 5
+    },
+    {
+      name: 'Scotch',
+      drunkness: 10
+    }
+  ];
+  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline
+  });
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// about page 
+app.get('/about', function (req, res) {
+  res.render('pages/about');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello<b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
